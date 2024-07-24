901cdc1a491920f856fb7d0999c383f407745127
ALsaead323 <alsaead2110679@miuegypt.edu.eg>
2024-07-24 15:15:10 +0300

    try to handle the Authorizaton

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,43 +1,48 @@
-import React, { lazy, Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from '../context/AuthContext';
+import FullLayout from '../layouts/FullLayout';
+import ProtectedRoute from '../components/ProtectedRoute';
 
-/****Layouts*****/
-const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+// Directly import components
+import Signup from '../components/Signup/Signup';
+import Signin from '../components/Signin/Signin';
+import About from '../views/About';
+import Alerts from '../views/ui/Alerts';
+import Badges from '../views/ui/Badges';
+import Buttons from '../views/ui/Buttons';
+import Cards from '../views/ui/Cards';
+import Grid from '../views/ui/Grid';
+import Tables from '../views/ui/Tables';
+import Forms from '../views/ui/Forms';
+import Breadcrumbs from '../views/ui/Breadcrumbs';
+import UserProfile from '../components/userProfile/userprofile';
 
-/***** Pages ****/
-const Signup = lazy(() => import("../components/Signup/Signup.js"));
-const Signin = lazy(() => import("../components/Signin/Signin.js"));
-const About = lazy(() => import("../views/About.js"));
-const Alerts = lazy(() => import("../views/ui/Alerts"));
-const Badges = lazy(() => import("../views/ui/Badges"));
-const Buttons = lazy(() => import("../views/ui/Buttons"));
-const Cards = lazy(() => import("../views/ui/Cards"));
-const Grid = lazy(() => import("../views/ui/Grid"));
-const Tables = lazy(() => import("../views/ui/Tables"));
-const Forms = lazy(() => import("../views/ui/Forms"));
-const Breadcrumbs = lazy(() => import("../views/ui/Breadcrumbs"));
-
-function AppRoutes() {
+const AppRoutes = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <AuthProvider>
       <Routes>
+        {/* Public Routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
+        {/* Protected Routes */}
         <Route path="/" element={<FullLayout />}>
           <Route index element={<Navigate to="/" />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/signin' element={<Signin />} />
-          <Route path="about" element={<About />} />
-          <Route path="alerts" element={<Alerts />} />
-          <Route path="badges" element={<Badges />} />
-          <Route path="buttons" element={<Buttons />} />
-          <Route path="cards" element={<Cards />} />
-          <Route path="grid" element={<Grid />} />
-          <Route path="table" element={<Tables />} />
-          <Route path="forms" element={<Forms />} />
-          <Route path="breadcrumbs" element={<Breadcrumbs />} />
+          <ProtectedRoute path="about" element={<About />} roles={['admin']} />
+          <ProtectedRoute path="alerts" element={<Alerts />} roles={['admin']} />
+          <ProtectedRoute path="badges" element={<Badges />} roles={['admin']} />
+          <ProtectedRoute path="buttons" element={<Buttons />} roles={['admin']} />
+          <ProtectedRoute path="cards" element={<Cards />} roles={['admin']} />
+          <ProtectedRoute path="grid" element={<Grid />} roles={['admin']} />
+          <ProtectedRoute path="table" element={<Tables />} roles={['admin']} />
+          <ProtectedRoute path="forms" element={<Forms />} roles={['admin']} />
+          <ProtectedRoute path="breadcrumbs" element={<Breadcrumbs />} roles={['admin']} />
         </Route>
+        <ProtectedRoute path="/profile" element={<UserProfile />} roles={['user']} />
       </Routes>
-    </Suspense>
+    </AuthProvider>
   );
-}
+};
 
 export default AppRoutes;
